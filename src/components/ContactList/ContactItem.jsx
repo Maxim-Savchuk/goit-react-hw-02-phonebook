26f849d35e5321953aee583737fb7423d9a597e7
maxim-savchuk <maxsavchuk1105@gmail.com>
2021-09-10 16:17:12 +0300

    test

--- src/components/ContactList/ContactItem.jsx
+++ src/components/ContactList/ContactItem.jsx
@@ -2,6 +2,9 @@ import s from './ContactList.module.css';
 
 export const ContactItem = ({ name, number }) => {
     return (
-        <li className={s.Item}>{name}: {number}</li>
+        <li className={s.Item}>
+            {name}: {number}
+            <button type="button" className={s.Button}>Delete</button>
+        </li>
     )
 }
